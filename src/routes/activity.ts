4b5944780f5c227a3384d4dbbadadf065a3f328d
Brandon Corey <bcorey85@gmail.com>
2020-09-17 14:16:25 -0500

    fix missing module bug

--- src/routes/activity.ts
+++ src/routes/activity.ts
@@ -1,21 +1,21 @@
-// import express from 'express';
-// import { validateRequest } from '../middleware/validateRequest';
-// import {
-// 	signup,
-// 	login,
-// 	forgotPassword,
-// 	resetPassword
-// } from '../controllers/auth';
-// import { emailRequired, passwordRequired } from '../validators';
-// import { updateUserPassword } from '../validators';
+import express from 'express';
+import { validateRequest } from '../middleware/validateRequest';
+import {
+	signup,
+	login,
+	forgotPassword,
+	resetPassword
+} from '../controllers/auth';
+import { emailRequired, passwordRequired } from '../validators';
+import { updateUserPassword } from '../validators';
 
-// const router = express.Router();
+const router = express.Router();
 
-// // router.post(
-// // 	'/signup',
-// // 	[ emailRequired, passwordRequired ],
-// // 	validateRequest,
-// // 	signup
-// // );
+// router.post(
+// 	'/signup',
+// 	[ emailRequired, passwordRequired ],
+// 	validateRequest,
+// 	signup
+// );
 
-// export { router as activityRouter };
+export { router as activityRouter };
