bbcdf32cf66f7632aa03c64c8c3699ec576e6a33
Brandon Corey <bcorey85@gmail.com>
2020-09-17 12:27:23 -0500

    change postgres connection on prod

--- knexfile.js
+++ knexfile.js
@@ -40,12 +40,7 @@ module.exports = {
 
 	production: {
 		client: 'pg',
-		connection: {
-			host: process.env.POSTGRES_HOST,
-			user: process.env.POSTGRES_USER,
-			password: process.env.POSTGRES_PASSWORD,
-			database: process.env.POSTGRES_DATABASE
-		},
+		connection: process.env.POSTGRES_URL,
 		pool: {
 			min: 2,
 			max: 10
