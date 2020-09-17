8530f22c3227d375adfcef1e0f7f56d123b75e7b
Brandon Corey <bcorey85@gmail.com>
2020-09-17 12:37:47 -0500

    fix config var for pg

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,5 @@ import { app } from './app';
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-	console.log(process.env.POSTGRES_URL);
 	console.log(`Server started on port ${port}`);
 });
